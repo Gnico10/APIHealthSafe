a02d0fe73e471381ac849ecf5e31f0341868f20f
Gastón Medina <gaston.medina@comparaencasa.com>
2022-04-16 15:47:47 -0300

    Definir campos devueltos

--- controllers/especialidades.ts
+++ controllers/especialidades.ts
@@ -3,20 +3,20 @@ import Especialidad from "../models/especialidad";
 
 // Get all Especialidades
 export const getEspecialidades = async (req: Request, res: Response) => {
-    const especialidades = await Especialidad.findAll();
+    const especialidades = await Especialidad.findAll({attributes: ['idespecialidad', 'descripcion']});
     res.json({especialidades});
 }
 
 // Get a single Especialidad by Id
 export const getEspecialidad = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const especialidad = await Especialidad.findByPk(id);
+    const especialidad = await Especialidad.findByPk(id, {attributes: ['idespecialidad', 'descripcion']});
 
     if (especialidad){
         res.json(especialidad);
     } else {
         res.status(404).json({
-            msg: `No existe un especialidad con ID = ${id}`
+            msg: `No existe una especialidad con ID = ${id}`
         });
     }
 }
