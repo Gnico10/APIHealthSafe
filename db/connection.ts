2a60db62624e51b68485d66d6ee8ad0aa127864d
Gastón Medina <gnico_10@hotmail.com>
2022-05-05 04:00:52 -0300

    heroku config

--- db/connection.ts
+++ db/connection.ts
@@ -7,12 +7,12 @@ let hostdb : string = process.env.HOSTDB || 'localhost';
 const portdb : string = process.env.PORTDB  || '5432';
 
 // Heroku Postgres config
-const herokudatabase : string = process.env.DATABASE_URL || '';
+let herokudatabase : string = process.env.DATABASE_URL || '';
 
 if (herokudatabase !== '') {
     //postgres://pmbfldzbowmrmv:9876c01f8ce8e6461979a7a7c845b8d756afe018dba3f390b02bc82139548b2e@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d9arna9nf226me
     console.log(`Heroku DB: ${herokudatabase}`);
-    herokudatabase.replace('postgres://', ''); // Remove postgres://
+    herokudatabase = herokudatabase.replace('postgres://', ''); // Remove postgres://
     usernamedb = herokudatabase.split(':')[0]; // Get username
     passworddb = herokudatabase.split(':')[1].split('@')[0]; // Get password
     hostdb = herokudatabase.split('@')[1].split(':')[0]; // Get host
@@ -28,7 +28,8 @@ const sequelize = new Sequelize(database, usernamedb, passworddb, {
     host: hostdb,
     dialect: 'postgres',
     // logging: false
-    port: (portdb as unknown as number)
+    port: (portdb as unknown as number),
+    ssl: true
 });
 
 export default sequelize;
