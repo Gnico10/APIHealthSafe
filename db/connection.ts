4c3b49a2ddc32610cae73445a19487016c58cc18
Gastón Medina <gnico_10@hotmail.com>
2022-05-05 03:39:49 -0300

    DB config

--- db/connection.ts
+++ db/connection.ts
@@ -7,7 +7,7 @@ let hostdb : string = process.env.HOSTDB || 'localhost';
 const portdb : string = process.env.PORTDB  || '5432';
 
 // Heroku Postgres config
-const herokudatabase : string = process.env.HEROKUDATABASE || '';
+const herokudatabase : string = process.env.DATABASE_URL || '';
 
 if (herokudatabase !== '') {
     //postgres://pmbfldzbowmrmv:9876c01f8ce8e6461979a7a7c845b8d756afe018dba3f390b02bc82139548b2e@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d9arna9nf226me
