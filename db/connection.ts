d195414cff889cf3e829ad9c6a526ec20379463f
Gastón Medina <gnico_10@hotmail.com>
2022-05-05 03:20:21 -0300

    Connect with heroku postgreql

--- db/connection.ts
+++ db/connection.ts
@@ -1,10 +1,22 @@
 import {Sequelize} from 'sequelize';
 
-const database : string = process.env.DATABASE || 'API';
-const usernamedb : string = process.env.USERNAMEDB || 'postgres';
-const passworddb : string = process.env.PASSWORDDB || 'postgres';
-const hostdb : string = process.env.HOSTDB || 'localhost';
+let database : string = process.env.DATABASE || 'API';
+let usernamedb : string = process.env.USERNAMEDB || 'postgres';
+let passworddb : string = process.env.PASSWORDDB || 'postgres';
+let hostdb : string = process.env.HOSTDB || 'localhost';
 const portdb : string = process.env.PORTDB  || '5432';
+
+// Heroku Postgres config
+const herokudatabase : string = process.env.HEROKUDATABASE || '';
+
+if (herokudatabase !== '') {
+    //postgres://pmbfldzbowmrmv:9876c01f8ce8e6461979a7a7c845b8d756afe018dba3f390b02bc82139548b2e@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d9arna9nf226me
+    herokudatabase.replace('postgres://', ''); // Remove postgres://
+    usernamedb = herokudatabase.split(':')[0]; // Get username
+    passworddb = herokudatabase.split(':')[1].split('@')[0]; // Get password
+    database = herokudatabase.split('@')[1].split(':')[0]; // Get database
+    hostdb = herokudatabase.split('@')[1].split('/')[1]; // Get host
+}
 
 const sequelize = new Sequelize(database, usernamedb, passworddb, {
     host: hostdb,
